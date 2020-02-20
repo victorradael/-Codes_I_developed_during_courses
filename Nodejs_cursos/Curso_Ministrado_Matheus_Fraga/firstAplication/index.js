10458f3dab575755f69f7d7323c31262aa802838
Victor Radael <victorradael@hotmail.com>
2020-02-19 23:08:00 -0300

    CRUD

--- Nodejs_cursos/Curso_Ministrado_Matheus_Fraga/firstAplication/index.js
+++ Nodejs_cursos/Curso_Ministrado_Matheus_Fraga/firstAplication/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?name=NodeJS
 // Route Params = /curso/2
 // Request Body = {name: "NodeJS", type: "backend"}
@@ -10,10 +12,65 @@ const cursos = ['NodeJS', 'JavaScript', 'React Native',];
 
 
 // localhost:3000/curso
-server.get('/curso/:index', (req, res) => {
+//Global Middleware
+server.use((req, res, next) => {
+    console.log(`Rota Chamada ${req.url}`);
+
+    return next();
+});
+
+function checkCurso(req, res, next) {
+    if (!req.body.name) {
+        return res.status(400).send({ error: 'Nome do curso é obrigatório' });
+    }
+
+    return next();
+}
+
+function checkIndexCurso(req, res, next) {
+    const curso = cursos[req.params.index];
+    if (!curso) {
+        return res.status(400).send({ error: 'Esse index não existe' });
+    }
+
+    return next();
+}
+
+// All courses
+server.get('/cursos', (req, res) => {
+
+    return res.json(cursos);
+});
+// Only one
+server.get('/cursos/:index', checkIndexCurso, (req, res) => {
     const { index } = req.params;
 
-    return res.json(cursos[index])
-})
+    return res.json(cursos[index]);
+});
+// add course
+server.post('/cursos', checkCurso, (req, res) => {
+    const { name } = req.body;
+    cursos.push(name);
+
+    return res.json(cursos);
+});
+// Att course
+server.put('/cursos/:index', checkCurso, checkIndexCurso, (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+});
+// Delete Course
+server.delete('/cursos/:index', checkIndexCurso , (req, res) => {
+    const { index } = req.params;
+
+    cursos.splice(index, 1);
+
+    return res.send();
+});
+
 
 server.listen(3000);
